b21e69e361537937ed79dd6b6caf924bfedcefc8
antoniouc <antoniouc38@gmail.com>
2024-06-03 23:19:28 -0600

    se le da formato a la fecha 'YYYY-MM-DD'

--- models/movimientomodel.js
+++ models/movimientomodel.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 class movimientos{
-    constructor(UsuarioID,Total,Descripcion,Fecha,CategoriaID,MetodoPagoID,Nota,Etiquetas,Tipo){
+    constructor(UsuarioID,Descripcion,Total,Fecha,CategoriaID,MetodoPagoID,Nota,Etiquetas,Tipo){
         this.UsuarioID = UsuarioID;
         this.Descripcion = Descripcion;
         this.Total = Total;
@@ -31,6 +31,49 @@ async function agregarMovimientos(UsuarioID,Descripcion,Total,Fecha,CategoriaID,
     }
 }
 
+async function obtenerMovimientos(usuarioID, token) {
+    const axiosConfig = {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    try {
+        const response = await axios.post(`${process.env.BASE_URL}/movimiento/historial`,{usuarioID}, axiosConfig);
+        // return response.data.map(movimiento => new movimientos(
+        //     movimiento.UsuarioID,
+        //     movimiento.Descripcion,
+        //     movimiento.Total,
+        //     movimiento.Fecha,
+        //     movimiento.CategoriaID,
+        //     movimiento.MetodoPagoID,
+        //     movimiento.Nota,
+        //     movimiento.Etiquetas,
+        //     movimiento.Tipo
+        // ));
+        return response.data.map(movimiento => {
+            // Convertir la fecha al formato "YYYY-MM-DD"
+            const fecha = new Date(movimiento.Fecha);
+            const fechaFormateada = fecha.toISOString().split('T')[0];
+            
+            return new movimientos(
+                movimiento.UsuarioID,
+                movimiento.Descripcion,
+                movimiento.Total,
+                fechaFormateada, // Usar la fecha formateada
+                movimiento.CategoriaID,
+                movimiento.MetodoPagoID,
+                movimiento.Nota,
+                movimiento.Etiquetas,
+                movimiento.Tipo
+            );
+        });
+    } catch (error) {
+        console.error('Error al obtener los movimientos:', error);
+        throw error;
+    }
+}
+
 module.exports={
-    agregarMovimientos
+    agregarMovimientos,
+    obtenerMovimientos
 }
